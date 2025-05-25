a49f20b686ca8da30b4100aa0a86bde2bb5faf27
trung <trungvt138@github.com>
2025-05-26 00:34:19 +0200

    finish DOM

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,1 +1,12 @@
 document.query
+// var third = document.firstElementChild.lastElementChild.getElementsByClassName("list").item(2);
+// third.innerHTML = "Trung Dam";
+
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+
+// var link = document.querySelector("li a");
+// link.style.color = "red";
+
+// document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
