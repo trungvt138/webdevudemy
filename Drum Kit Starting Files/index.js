fa76750d6a825c3d5fab24e247ea2a02179b4385
trung <trungvt138@github.com>
2025-05-27 22:28:33 +0200

    add switch case for different sounds

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,40 @@
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('click', (e) => {
-        alert("I got clicked");
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (i = 0; i < numOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (event) {
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "a":
+                new Audio("sounds/tom-2.mp3").play();
+                break;
+
+            case "w":
+                new Audio("sounds/tom-1.mp3").play();
+                break;
+
+            case "s":
+                new Audio("sounds/tom-3.mp3").play();
+                break;
+
+            case "d":
+                new Audio("sounds/tom-4.mp3").play();
+                break;
+
+            case "j":
+                new Audio("sounds/crash.mp3").play();
+                break;
+
+            case "k":
+                new Audio("sounds/kick-bass.mp3").play();
+                break;
+
+            case "l":
+                new Audio("sounds/snare.mp3").play();
+                break;
+        }
     })
-})
+}
+
+// const audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
