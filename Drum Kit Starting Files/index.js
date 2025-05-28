177933467c511c0573d3f7c52a051211e6d25bdb
trung <trungvt138@github.com>
2025-05-28 02:21:13 +0200

    add keydown event listener

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,38 +3,42 @@ var numOfDrumButtons = document.querySelectorAll(".drum").length;
 for (i = 0; i < numOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function (event) {
         var buttonInnerHTML = this.innerHTML;
-
-        switch (buttonInnerHTML) {
-            case "a":
-                new Audio("sounds/tom-2.mp3").play();
-                break;
-
-            case "w":
-                new Audio("sounds/tom-1.mp3").play();
-                break;
-
-            case "s":
-                new Audio("sounds/tom-3.mp3").play();
-                break;
-
-            case "d":
-                new Audio("sounds/tom-4.mp3").play();
-                break;
-
-            case "j":
-                new Audio("sounds/crash.mp3").play();
-                break;
-
-            case "k":
-                new Audio("sounds/kick-bass.mp3").play();
-                break;
-
-            case "l":
-                new Audio("sounds/snare.mp3").play();
-                break;
-        }
+        playDrumSound(buttonInnerHTML);
     })
 }
 
-// const audio = new Audio('sounds/tom-1.mp3');
-// audio.play();
+document.addEventListener("keydown", function (event) {
+    playDrumSound(event.key);
+})
+
+function playDrumSound(buttonInnerHTML) {
+    switch (buttonInnerHTML) {
+        case "a":
+            new Audio("sounds/tom-2.mp3").play();
+            break;
+
+        case "w":
+            new Audio("sounds/tom-1.mp3").play();
+            break;
+
+        case "s":
+            new Audio("sounds/tom-3.mp3").play();
+            break;
+
+        case "d":
+            new Audio("sounds/tom-4.mp3").play();
+            break;
+
+        case "j":
+            new Audio("sounds/crash.mp3").play();
+            break;
+
+        case "k":
+            new Audio("sounds/kick-bass.mp3").play();
+            break;
+
+        case "l":
+            new Audio("sounds/snare.mp3").play();
+            break;
+    }
+}
