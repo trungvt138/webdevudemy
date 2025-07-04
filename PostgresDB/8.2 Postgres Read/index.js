de44015f2b4704b52f5a8e7f094ae19d4f857385
trung <trungvt138@github.com>
2025-07-04 21:54:03 +0200

    finish 8.2

--- PostgresDB/8.2 Postgres Read/index.js
+++ PostgresDB/8.2 Postgres Read/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -10,21 +11,41 @@ let totalCorrect = 0;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "123456",
+  port: 5432,
+});
+
 let currentQuestion = {};
+let quiz = [];
+
+db.connect();
+db.query("SELECT * FROM flags", (err, result) => {
+  if (err) {
+    console.log("Error executing query", err.stack);
+  } else {
+    quiz = result.rows;
+  }
+  db.end();
+})
 
 // GET home page
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   totalCorrect = 0;
-  nextQuestion();
+  await nextQuestion();
   console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
+  res.render("index.ejs", {question: currentQuestion});
 });
 
 // POST a new post
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  const correctAnswer = currentQuestion.name;
+  if (correctAnswer.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
@@ -35,6 +56,7 @@ app.post("/submit", (req, res) => {
     question: currentQuestion,
     wasCorrect: isCorrect,
     totalScore: totalCorrect,
+    correctAnswer: correctAnswer,
   });
 });
 
