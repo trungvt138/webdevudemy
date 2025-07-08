1bf17891ef4b5da5506d7dd864b29ebfbf2ffc07
trung <trungvt138@github.com>
2025-07-08 22:51:11 +0200

    finish 8.5. come back to optimize

--- PostgresDB/8.5 Family Travel Tracker/index.js
+++ PostgresDB/8.5 Family Travel Tracker/index.js
@@ -25,37 +25,52 @@ let users = [
 ];
 
 async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+  const result = await db.query(
+      "SELECT country_code FROM visited_countries " +
+      "INNER JOIN users " +
+      "ON user_id = users.id " +
+      "WHERE user_id = $1", [currentUserId]
+  );
   let countries = [];
-  result.rows.forEach((country) => {
-    countries.push(country.country_code);
+  result.rows.forEach((row) => {
+    countries.push(row.country_code);
   });
   return countries;
 }
+
+async function getCurrentUser() {
+  const result = await db.query("SELECT * FROM users");
+  users = result.rows;
+  return users.find((user) => user.id == currentUserId);
+}
+
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
+  const currentUser = await getCurrentUser();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
     users: users,
-    color: "teal",
-  });
+    color: currentUser.color
+  })
 });
+
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
   try {
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '_%' || $1 || '%';",
       [input.toLowerCase()]
     );
 
     const data = result.rows[0];
     const countryCode = data.country_code;
+    console.log(countryCode);
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
       );
       res.redirect("/");
     } catch (err) {
@@ -65,11 +80,35 @@ app.post("/add", async (req, res) => {
     console.log(err);
   }
 });
-app.post("/user", async (req, res) => {});
+
+app.post("/user", async (req, res) => {
+  //console.log(req.body);
+  if (req.body["user"]) {
+    currentUserId = req.body["user"];
+    res.redirect("/");
+  } else {
+    res.render("new.ejs", {})
+  }
+});
 
 app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
   //https://www.postgresql.org/docs/current/dml-returning.html
+  const inputName = req.body["name"];
+  const inputColor = req.body["color"];
+
+  //console.log(inputColor);
+
+  const result = await db.query(
+      "INSERT INTO users (name, color) VALUES ($1,$2)" +
+      "RETURNING *",
+      [inputName, inputColor]
+  );
+
+  const id = result.rows[0].id;
+  currentUserId = id;
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
