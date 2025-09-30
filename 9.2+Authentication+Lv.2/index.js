5bf025ee00e931d6137021df01ab554e18a822f7
trung <trungvt138@github.com>
2025-10-01 00:37:50 +0200

    finish 9.1

--- 9.2+Authentication+Lv.2/index.js
+++ 9.2+Authentication+Lv.2/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "users",
+  database: "secrets",
   password: "123456",
   port: 5432,
 });
@@ -35,13 +35,15 @@ app.post("/register", async (req, res) => {
 
   try {
     const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
-        email,
+      email,
     ]);
+
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-      const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)",
-          [email, password]
+      const result = await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        [email, password]
       );
       console.log(result);
       res.render("secrets.ejs");
@@ -57,7 +59,7 @@ app.post("/login", async (req, res) => {
 
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
-        email,
+      email,
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
@@ -66,7 +68,7 @@ app.post("/login", async (req, res) => {
       if (password === storedPassword) {
         res.render("secrets.ejs");
       } else {
-        res.send("Incorrect password.");
+        res.send("Incorrect Password");
       }
     } else {
       res.send("User not found");
